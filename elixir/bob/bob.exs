9a2b7877a0c6382dcd6c88ac0188346e3a39aa80
Angelos Orfanakos <me@agorf.gr>
2016-02-18 00:22:52 +0200

    Add Elixir solution to Bob problem

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,13 @@
+defmodule Bob do
+  def hey(input) do
+    clean_input = Regex.replace(~r/[^\w?]/u, input, "")
+
+    cond do
+      String.ends_with?(clean_input, "?") -> "Sure."
+      String.match?(clean_input, ~r/\A[^a-z]+\z/) &&
+        !String.match?(clean_input, ~r/\A\d+\z/) -> "Whoa, chill out!"
+      clean_input == "" -> "Fine. Be that way!"
+      true -> "Whatever."
+    end
+  end
+end
