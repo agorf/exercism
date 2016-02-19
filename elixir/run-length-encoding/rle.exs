592b32e6a7ef0fefb37adeee7f89298b1496ce3a
Angelos Orfanakos <me@agorf.gr>
2016-02-19 23:15:20 +0200

    Improve Elixir Run Length Encoding solution
    - Use `Enum.map_join` instead of `Enum.map` and `Enum.join`
    - Remove parentheses around `fn` parameters
    - Break long lines
    - Remove indentation from pipe operator
    - Use `capture: :all_but_first` to skip first match from `Regex.scan`

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -6,17 +6,19 @@ defmodule RunLengthEncoder do
   It should also be able to reconstruct the data into its original form. 
   "1H1O1R1S1E" => "HORSE" 
   """
-  @spec encode(string) :: String.t
+  @spec encode(String.t) :: String.t
   def encode(string) do
     Regex.scan(~r/([A-Z])\1*/, string)
-      |> Enum.map(fn([whole, c]) -> [String.length(whole), c] |> Enum.join end)
-      |> Enum.join
+    |> Enum.map_join(fn [whole, char] ->
+      Enum.join([String.length(whole), char])
+    end)
   end
 
-  @spec decode(string) :: String.t
+  @spec decode(String.t) :: String.t
   def decode(string) do
-    Regex.scan(~r/(\d+)([A-Z])/, string)
-      |> Enum.map(fn([_, n, str]) -> String.duplicate(str, String.to_integer(n)) end)
-      |> Enum.join
+    Regex.scan(~r/(\d+)([A-Z])/, string, capture: :all_but_first)
+    |> Enum.map_join(fn [count, char] ->
+      String.duplicate(char, String.to_integer(count))
+    end)
   end
 end
