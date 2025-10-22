15acf7841b45c16ab111262e3cfb9e31cd5ed547
Angelos Orfanakos <git@angelos.dev>
2025-10-22 19:25:20 +0300

    [Go] Add solution for vehicle-purchase

---
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,30 @@
+package purchase
+
+import "fmt"
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	return kind == "car" || kind == "truck"
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	choice := option1
+	if option1 > option2 {
+		choice = option2
+	}
+	return fmt.Sprintf("%s is clearly the better choice.", choice)
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	if age < 3 {
+		return 0.8 * originalPrice
+	}
+
+	if age >= 10 {
+		return 0.5 * originalPrice
+	}
+
+	return 0.7 * originalPrice
+}
